ae1b7c120b1c682315a676a86c9bc194bdb59a8e
srishti-maurya <srishti22maurya@gmail.com>
2021-08-13 21:50:21 +0530

    added functionality to calulate area and quiz

--- src/pages/areaCalculator.jsx
+++ src/pages/areaCalculator.jsx
@@ -1,9 +1,242 @@
-import React from "react";
-
+import React, { useState } from "react";
+let base,
+  height, side1, side2, side3, length1, length2, angle;
 function AreaCalculator() {
+  const [option1OutputMessage, setOption1OutputMessage] = useState(["", 0]);
+  const [option2OutputMessage, setOption2OutputMessage] = useState(["", 0]);
+  const [option3OutputMessage, setOption3OutputMessage] = useState(["", 0]);
+  function choseOptionHandler() {
+    if (document.getElementById("option1").checked) {
+      document.getElementById("option1Body").style.display = "block";
+      document.getElementById("option2Body").style.display = "none";
+      document.getElementById("option3Body").style.display = "none";
+    } else if (document.getElementById("option2").checked) {
+      document.getElementById("option1Body").style.display = "none";
+      document.getElementById("option2Body").style.display = "block";
+      document.getElementById("option3Body").style.display = "none";
+    } else if (document.getElementById("option3").checked) {
+      document.getElementById("option1Body").style.display = "none";
+      document.getElementById("option2Body").style.display = "none";
+      document.getElementById("option3Body").style.display = "block";
+    }
+  }
+
+  function option1Handler(event) {
+    event.preventDefault();
+    const area1 = (Number(base) * Number(height)) / 2;
+    setOption1OutputMessage(["Area = ", area1]);
+  }
+
+  function option2Handler(event) {
+    event.preventDefault();
+    const a = Number(side1);
+    const b = Number(side2);
+    const c = Number(side3);
+    const semiPeri = (a + b + c) / 2;
+    const ar = (semiPeri - a) * (semiPeri - b) * (semiPeri - c) * semiPeri;
+    const area2 = Math.sqrt(ar);
+    setOption2OutputMessage(["Area = ", area2]);
+  }
+
+  function option3Handler(event) {
+    event.preventDefault();
+    const b = Number(length1);
+    const c = Number(length2);
+    let a =Number(angle)
+    a = a * Math.PI / 180;
+    const area3 = ((b*c*Math.sin(a))*(1/2));
+    setOption3OutputMessage(["Area = ", area3]);
+  }
   return (
-    <div>
-      <h1>Area Calculator</h1>
+    <div className="container">
+      <div className="triangle-angle">
+        <h1>Calculate Area</h1>
+        <h4 className="triangle-angle-heading">
+          Select an option below as per the data you have for a triangle
+        </h4>
+
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="option"
+            id="option1"
+            onClick={choseOptionHandler}
+          />
+          <label class="form-check-label" for="option1">
+            If you have base and height length
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="option"
+            id="option2"
+            onClick={choseOptionHandler}
+          />
+          <label class="form-check-label" for="option2">
+            If you have length of 3 sides
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="option"
+            id="option3"
+            onClick={choseOptionHandler}
+          />
+          <label class="form-check-label" for="option3">
+            If you have length of 2 sides and included angle
+          </label>
+        </div>
+        <div id="option1Body" style={{ display: "none" }}>
+          <div className="container">
+            <div className="triangle-angle">
+              <h4 className="triangle-angle-heading">
+                Enter the base and height of the triangle
+              </h4>
+              <form onSubmit={option1Handler}>
+                <div class="row">
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="base"
+                      onChange={(e) => {
+                        base = e.target.value;
+                      }}
+                    />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="height"
+                      onChange={(e) => {
+                        height = e.target.value;
+                      }}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  class="btn btn-primary triangle-angle-btn"
+                >
+                  Submit
+                </button>
+              </form>
+
+              <div>{option1OutputMessage}</div>
+            </div>
+          </div>
+        </div>
+        <div id="option2Body" style={{ display: "none" }}>
+          <div className="container">
+            <div className="triangle-angle">
+              <h4 className="triangle-angle-heading">
+                Enter the sides in below input boxes to calculate the area of
+                the triangle
+              </h4>
+              <form onSubmit={option2Handler}>
+                <div class="row">
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Side 1"
+                      onChange={(e) => {
+                        side1 = e.target.value;
+                      }}
+                    />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Side 2"
+                      onChange={(e) => {
+                        side2 = e.target.value;
+                      }}
+                    />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Side 3"
+                      onChange={(e) => {
+                        side3 = e.target.value;
+                      }}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  class="btn btn-primary triangle-angle-btn"
+                >
+                  Submit
+                </button>
+              </form>
+
+              <div>{option2OutputMessage}</div>
+            </div>
+          </div>
+        </div>
+        <div id="option3Body" style={{ display: "none" }}>
+        <div className="container">
+            <div className="triangle-angle">
+              <h4 className="triangle-angle-heading">
+                Enter the 2 sides and angle in below input boxes to calculate the area of
+                the triangle
+              </h4>
+              <form onSubmit={option3Handler}>
+                <div class="row">
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Side 1"
+                      onChange={(e) => {
+                        length1 = e.target.value;
+                      }}
+                    />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Side 2"
+                      onChange={(e) => {
+                        length2 = e.target.value;
+                      }}
+                    />
+                  </div>
+                  <div class="col">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="Angle"
+                      onChange={(e) => {
+                        angle = e.target.value;
+                      }}
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  class="btn btn-primary triangle-angle-btn"
+                >
+                  Submit
+                </button>
+              </form>
+
+              <div>{option3OutputMessage}</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,9 +1,186 @@
+import { useState } from "react";
 import React from "react";
 
 function Quiz() {
+  let counter = 0;
+  const [score, setScore]=useState(0);
+  function btnHandler(e) {
+    e.preventDefault();
+    if (document.getElementById("ques1-1").checked) {
+      counter++;
+      document.getElementById("block1").style.backgroundColor="#b8e6be";
+
+    }
+    if (document.getElementById("ques2-2").checked) {
+      counter++;
+      document.getElementById("block2").style.backgroundColor="#b8e6be";
+    }
+    if (document.getElementById("ques3-1").checked) {
+      counter++;
+      document.getElementById("block3").style.backgroundColor="#b8e6be";
+    }
+    if (document.getElementById("ques4-1").checked) {
+      counter++;
+      document.getElementById("block4").style.backgroundColor="#b8e6be";
+    }
+    if (document.getElementById("ques5-1").checked) {
+      counter++;
+      document.getElementById("block5").style.backgroundColor="#b8e6be";
+    }
+    setScore(counter);
+    document.getElementById("scoreDisplay").style.display = "block";
+  }
+
   return (
     <div>
       <h1>Quiz</h1>
+      <form onSubmit={btnHandler}>
+        <div className="quesBlock" id="block1">
+          <p>
+            1. If a triangle has angles 135°, 15°, 30°. Is it an obtuse
+            triangle?
+          </p>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques1"
+              id="ques1-1"
+            />
+            <label className="form-check-label" for="ques1-1">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques1"
+              id="ques1-2"
+            />
+            <label className="form-check-label" for="ques1-2">
+              No
+            </label>
+          </div>
+        </div>
+        <div className="quesBlock" id="block2">
+          <p>
+            2. If a triangle has angles 115°, 25°, 40°. Is it an acute triangle?
+          </p>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques2"
+              id="ques2-1"
+            />
+            <label className="form-check-label" for="ques1-1">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques2"
+              id="ques2-2"
+            />
+            <label className="form-check-label" for="ques1-2">
+              No
+            </label>
+          </div>
+        </div>
+        <div className="quesBlock" id="block3">
+          <p>
+            3. If a triangle has angles 90°, 60°, 30°. Is it a right angle
+            triangle?
+          </p>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques3"
+              id="ques3-1"
+            />
+            <label className="form-check-label" for="ques1-1">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques3"
+              id="ques3-2"
+            />
+            <label className="form-check-label" for="ques1-2">
+              No
+            </label>
+          </div>
+        </div>
+        <div className="quesBlock" id="block4">
+          <p>
+            4. A triangle has angles 60°, 60°, 60°. Is it an equilateral
+            triangle?
+          </p>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques4"
+              id="ques4-1"
+            />
+            <label className="form-check-label" for="ques1-1">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques4"
+              id="ques4-2"
+            />
+            <label className="form-check-label" for="ques1-2">
+              No
+            </label>
+          </div>
+        </div>
+        <div className="quesBlock" id="block5">
+          <p>
+            5. If a triangle has angles 25°, 75°, 80°. Is it an acute triangle?
+          </p>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques5"
+              id="ques5-1"
+            />
+            <label className="form-check-label" for="ques1-1">
+              Yes
+            </label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="ques5"
+              id="ques5-2"
+            />
+            <label className="form-check-label" for="ques1-2">
+              No
+            </label>
+          </div>
+        </div>
+        <button className="btn btn-primary" style={{ margin: "2rem" }}>
+          Submit
+        </button>
+      </form>
+
+      <div id="scoreDisplay" style={{ display: "none" }}>
+        Your total score is : {score}
+      </div>
     </div>
   );
 }
